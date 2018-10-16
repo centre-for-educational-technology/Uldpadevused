d1ed937b9485fde4a57275946ab3374604656dd6
leoste <karl.leoste@gmail.com>
2018-10-16 13:45:44 +0300

    lugmot start

---
+++ actions/lugmot/sobiv2.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+
+$wcode = get_input('wcode');
+$page = get_input('page');
+
+//save data to session
+$value = get_input('q'.'1');
+$_SESSION[$wcode.'p2q'.'1'] = $value;
+
+//go to the next question
+forward_next_url($wcode, $page);
---
+++ views/default/forms/lugmot/sobiv2.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+//extract data to put in hidden fields
+$wcode = elgg_extract('wcode', $vars);
+$page = elgg_extract('page', $vars);
+$maxp = elgg_extract('maxp', $vars);
+//make hidden fields
+form_view_hidden_fields($wcode, $page);
+
+$title = 'Vali sulle sobiv vastus!';
+$labels = [
+  'Lugeda on tore.'
+];
+
+echo elgg_view_title($title);
+form_view_radios($labels, $wcode, $page);
+
+//make appropriate buttons in the end
+form_view_buttons($wcode, $page, $maxp);
