95ee66ed2a3f1752c1f35d87a358e95303720635
leoste <karl.leoste@gmail.com>
2018-12-14 14:10:24 +0200

    Resolved #61

--- start.php
+++ start.php
@@ -106,7 +106,7 @@ const worksheets = [
     'name' => 'Matemaatika ülesanded 4. klass',
     'file' => 'sheets/maths',
     'pages' => [
-      1 => 2, 2 => 2, 3 => 2, 4 => 2
+      1 => 3, 2 => 3, 3 => 3, 4 => 3
     ],
     'timelimit' => 86400,
     'alias' => 'maths'
@@ -115,7 +115,7 @@ const worksheets = [
     'name' => 'Matemaatika ülesanded 2. klass',
     'file' => 'sheets/maths2',
     'pages' => [
-      1 => 2, 2 => 2, 3 => 2, 4 => 2
+      1 => 3, 2 => 3, 3 => 3, 4 => 3
     ],
     'timelimit' => 86400,
     'alias' => 'maths2kl'
