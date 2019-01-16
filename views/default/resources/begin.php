e868518b79a79fcfa270ce06999bec533ac742d3
leoste <karl.leoste@gmail.com>
2019-01-16 13:07:52 +0200

    quick fix

--- views/default/resources/begin.php
+++ views/default/resources/begin.php
@@ -20,17 +20,3 @@ $title = ee_echo('polls:main:title');
 $content = '<div id="image">'.view_img('statue.jpg').'</div>';
 $content .= elgg_view_form("begin");
 echo elgg_view_page($title, $content);
-
-/*elgg_load_css('hidebar');
-elgg_load_css('general');
-elgg_load_css('begin');
-
-$title = ee_echo('polls:main:title');;
-$content = elgg_view_title($title);
-$content .= elgg_view_form("begin");
-
-$body = elgg_view_layout('no_sidebar', array(
-   'content' => $content
-));
-
-echo elgg_view_page($title, $body);*/
