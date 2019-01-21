ab864b3d3736c10618a23d9676b25c9062487441
leoste <karl.leoste@gmail.com>
2019-01-21 12:51:44 +0200

    improved chain design

--- views/default/forms/sheets/chain.php
+++ views/default/forms/sheets/chain.php
@@ -14,34 +14,14 @@ $c = $page > 2 ? 6 : 4;
 $ltr = "abcdef";
 $url = elgg_get_site_url();
 
-echo '<img src="'.$url.'mod/Uldpadevused/images/kett'.$page.'.png" style="width:100%"><br>';
+echo '<img class="big-img" src="'.$url.'mod/Uldpadevused/images/kett'.$page.'.png"><br>';
 
-echo "Vali sobiv:";
+echo "<p>Vali sobiv:</p>";
 
 for ($i = 0; $i < $c; $i += 1)
 {
-    $options['<img src="'.$url.'mod/Uldpadevused/images/kett'.$page.$ltr[$i].'.png" style="width:calc(100% / 6);">'] = $i + 1;
+    $options['<img class="small-img" src="'.$url.'mod/Uldpadevused/images/kett'.$page.$ltr[$i].'.png">'] = $i + 1;
 }
-
-//technically this is not the correct way of doing css in elgg
-echo '<style>
-.elgg-input-radios.elgg-horizontal li{
-    padding-right:0
-  }
-  input[type="radio"]{
-    opacity:0;
-    position:fixed;
-    margin:0;
-  }
-  input[type="radio"]:not(:checked)+img{
-    -filter:sepia(100%) saturate(4000%) grayscale(100%);
-    -webkit-filter:sepia(100%) saturate(4000%) grayscale(100%);
-  }
-  input[type="radio"]:checked+img{
-    -filter:sepia(100%) saturate(4000%) grayscale(80%) hue-rotate(300deg);
-    -webkit-filter:sepia(100%) saturate(4000%) grayscale(80%) hue-rotate(300deg);
-  }
-</style>';
 
 echo elgg_view_field([
     'name' => 'q1',
